78eec7cf88238c875332a1f57d5b742550f2b863
ZL&EL <lzc123456ab@gmail.com>
2023-07-15 21:59:04 -0400

    01

---
+++ hello.js
@@ -0,0 +1,15 @@
+// console.log("hello world");
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = "Kumar";
+console.log(found);
+console.log("hi ".concat(firstName, " ").concat(lastName));
+var review = [1, 4, 6, 3, 5, 2];
+var s = ["Anup", "Kumar", "Kumar"];
+s.push("Kumar");
+s.push("fdfdfd");
+for (var _i = 0, s_1 = s; _i < s_1.length; _i++) {
+    var q = s_1[_i];
+    console.log(q);
+}
---
+++ hello.ts
@@ -0,0 +1,15 @@
+// console.log("hello world");
+let found :boolean = true;
+let grade :number = 88.6;
+let firstName:string = "Anup";
+let lastName:string = "Kumar";
+console.log(found);
+
+console.log(`hi ${firstName} ${lastName}`);
+let review: number[] = [1,4,6,3,5,2];
+let s: string[] = ["Anup","Kumar","Kumar"];
+s.push("Kumar");
+s.push("fdfdfd");
+for(let q of s){
+    console.log(q);
+}
