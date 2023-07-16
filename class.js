90cc11bd10fbdfa3ef63c79e7185d1a7d7024b11
ZL&EL <lzc123456ab@gmail.com>
2023-07-15 22:06:13 -0400

    2

--- class.js
+++ class.js
@@ -1,30 +1,22 @@
-var Customer = /** @class */ (function () {
-    function Customer(firstName, lastName) {
+"use strict";
+class Customer {
+    constructor(firstName, lastName) {
         this._firstName = firstName;
         this._lastName = lastName;
     }
-    Object.defineProperty(Customer.prototype, "firstName", {
-        get: function () {
-            return this._firstName;
-        },
-        set: function (value) {
-            this._firstName = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Customer.prototype, "lastName", {
-        get: function () {
-            return this._lastName;
-        },
-        set: function (value) {
-            this._lastName = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Customer;
-}());
-var myCustomer = new Customer("Martin", "Dixon");
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(value) {
+        this._firstName = value;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        this._lastName = value;
+    }
+}
+let myCustomer = new Customer("Martin", "Dixon");
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
--- hello.js
+++ hello.js
@@ -1,15 +1,15 @@
+"use strict";
 // console.log("hello world");
-var found = true;
-var grade = 88.6;
-var firstName = "Anup";
-var lastName = "Kumar";
+let found = true;
+let grade = 88.6;
+let firstName = "Anup";
+let lastName = "Kumar";
 console.log(found);
-console.log("hi ".concat(firstName, " ").concat(lastName));
-var review = [1, 4, 6, 3, 5, 2];
-var s = ["Anup", "Kumar", "Kumar"];
+console.log(`hi ${firstName} ${lastName}`);
+let review = [1, 4, 6, 3, 5, 2];
+let s = ["Anup", "Kumar", "Kumar"];
 s.push("Kumar");
 s.push("fdfdfd");
-for (var _i = 0, s_1 = s; _i < s_1.length; _i++) {
-    var q = s_1[_i];
+for (let q of s) {
     console.log(q);
 }
