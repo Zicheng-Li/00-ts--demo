2a1cd2b2a0240217801ed376204c9a646f831417
ZL&EL <lzc123456ab@gmail.com>
2023-07-16 15:26:34 -0400

    2

--- Driver.ts
+++ Driver.ts
@@ -1,4 +1,14 @@
-import {Customer} from './class';
-let myCustomer = new Customer("Martin", "Dixon");
-console.log(myCustomer.firstName);
-console.log(myCustomer.lastName);
+// import {Customer} from './class';
+// let myCustomer = new Customer("Martin", "Dixon");
+// console.log(myCustomer.firstName);
+// console.log(myCustomer.lastName);
+import {Shape} from './Shape';
+import {Circle} from './Circle';
+import {Rectangle} from './Rectangle';
+
+let myShape = new Shape(10, 15);
+console.log(myShape.getInfo());
+let myCircle = new Circle(5, 10, 20);
+console.log(myCircle.getInfo());
+let myRectangle = new Rectangle(0, 0, 3, 7);
+console.log(myRectangle.getInfo());
